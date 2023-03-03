07fadac2b795d7fac6163f80f47db2ef2966587b
DevGla <george.almeida@grupoboticario.com.br>
2023-03-03 11:08:33 -0300

    refactor: Adicionando RequestParam

--- Rest-Spring/src/main/kotlin/Projeto/RestSpring/GreetingController.kt
+++ Rest-Spring/src/main/kotlin/Projeto/RestSpring/GreetingController.kt
@@ -1,6 +1,7 @@
 package Projeto.RestSpring
 
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
@@ -9,7 +10,7 @@ class GreetingController {
 
     val counter: AtomicLong = AtomicLong()
     @RequestMapping("/greeting")
-    fun greeting(): Greeting {
-        return Greeting(counter.incrementAndGet(), "Hello World")
+    fun greeting(@RequestParam(value= "name", defaultValue = "World") name: String?): Greeting {
+        return Greeting(counter.incrementAndGet(), "Hello $name")
     }
 }
