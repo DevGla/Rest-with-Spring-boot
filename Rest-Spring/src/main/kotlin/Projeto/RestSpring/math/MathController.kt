af1df422d885ad205f8db59f7033e56c06a921a6
DevGla <glalmeida.dev@gmail.com>
2023-03-05 15:59:12 -0300

    feat: operation

--- Rest-Spring/src/main/kotlin/Projeto/RestSpring/math/MathController.kt
+++ Rest-Spring/src/main/kotlin/Projeto/RestSpring/math/MathController.kt
@@ -9,24 +9,57 @@ import java.lang.Exception
 @RestController
 class MathController {
     // RequestMaping permite uma ligacão entre o Path e o método manipulador(nesse caso: sum)
-    @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
+
 
     // PathVariable nos permite recuperar dados que foram passados na URL e usá-los
+
+    @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
     fun sum(
         @PathVariable(value = "numberOne") numberOne: String,
         @PathVariable(value = "numberTwo") numberTwo: String
     ): Double {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw Exception()
-        return convertDouble(numberOne) + convertDouble(numberTwo)
+        return convertDouble(numberOne) + convertDouble(
+            numberTwo
+        )
     }
 
-    private fun isNumeric(number: String?): Boolean {
+    @RequestMapping(value = ["/subtra/{numberOne}/{numberTwo}"])
+    fun subtra(
+        @PathVariable(value = "numberOne") numberOne: String,
+        @PathVariable(value = "numberTwo") numberTwo: String
+    ): Double {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw Exception()
+        return convertDouble(numberOne) - convertDouble(
+            numberTwo
+        )
+    }
+
+    // PathVariable nos permite recuperar dados que foram passados na URL e usá-los
+    @RequestMapping(value = ["/mult/{numberOne}/{numberTwo}"])
+    fun mult(
+        @PathVariable(value = "numberOne") numberOne: String,
+        @PathVariable(value = "numberTwo") numberTwo: String
+    ): Double {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw Exception()
+        return convertDouble(numberOne) * convertDouble(
+            numberTwo
+        )
+    }
+    @RequestMapping(value = ["/div/{numberOne}/{numberTwo}"])
+// PathVariable nos permite recuperar dados que foram passados na URL e usá-los
+    fun div(
+        @PathVariable(value = "numberOne") numberOne: String,
+        @PathVariable(value = "numberTwo") numberTwo: String
+    ): Double {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw Exception()
+        return convertDouble(numberOne) / convertDouble(numberTwo)
+    }
+    fun isNumeric(number: String?): Boolean {
         if(number.isNullOrBlank()) return false
         val reNumber = number.replace(",".toRegex(), ".")
         return reNumber.matches("""[-+]?[0-9]*\.?[0-9]+""".toRegex())
-
     }
-
     private fun convertDouble(number: String?): Double {
         if(number.isNullOrBlank()) return 0.0
         val reNumber = number.replace(",".toRegex(), ".")
